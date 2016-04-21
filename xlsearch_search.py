9f0efc507eb52480d27608fd3526a43eee2bb652
Sujun Li <lisujun@gmail.com>
2016-04-21 16:15:15 -0400

    updated version of 04/21/2016

---
+++ xlsearch_search.py
@@ -0,0 +1,111 @@
+import sys
+import pickle
+import getopt
+from time import ctime
+
+usage = '''
+USAGE: python xlsearch_search.py -l [path to xlsearch library]
+                                 -p [parameter file]
+                                 -o [output file]'''
+
+(pairs, args) = getopt.getopt(sys.argv[1:], 'l:p:o:')
+cmd_arg = dict()
+for i in range(len(pairs)):
+	cmd_arg[pairs[i][0]] = pairs[i][1]
+
+if len(cmd_arg) != 3:
+	print usage
+	sys.exit(1)
+
+lib_path = cmd_arg['-l']
+param_file = cmd_arg['-p']
+output_file = cmd_arg['-o']
+
+sys.path.append(lib_path)
+
+from utility import *
+from index import EnumIndexBuilder
+from match import Match
+
+print 'XLSearch, version 1.0'
+print 'Copyright of School of Informatics and Computing, Indiana University'
+print 'Current time: %s' % ctime() 
+
+print 'Performing database search for identifying cross-linked peptides...'
+
+print '\nReading paramters from: %s...' % param_file
+[param, mass] = read_param(param_file)
+print 'Reading parameters done!'
+
+print '\nReading MSMS spectra files from directory: %s...' % param['ms_data']
+spec_dict = read_spec(param['ms_data'], param, mass)
+pickle.dump(spec_dict, file('spectra.pickle', 'w'))
+print 'Total number of spectra: %d' % len(spec_dict)
+print 'Reading MSMS spectra files done!'
+
+if param['deisotope'] == True:
+	print '\nDeisotoping MSMS spectra...'
+	spec_dict = pickle.load(file('spectra.pickle'))
+	deisotoped = dict()
+	titles = spec_dict.keys()
+
+	ms2tol = 0.02 # 0.02Da for deisotoping
+
+	for i in range(len(titles)):
+		title = titles[i]
+		(one, align) = spec_dict[title].deisotope(mass, param['ndeisotope'], ms2tol)
+		deisotoped[title] = one
+
+	pickle.dump(deisotoped, file('deisotoped.pickle', 'w'))
+	deisotoped = pickle.load(file('deisotoped.pickle'))
+	spec_dict = deisotoped
+
+	print 'Deisotoping MSMS spectra done!'
+	print 'Current time: %s' % ctime()
+
+print '\nBuilding index for all possible inter-peptide cross-links...'
+index = EnumIndexBuilder(param['database'], spec_dict, mass, param)
+pickle.dump(index, file('index.pickle', 'w'))
+index = pickle.load(file('index.pickle'))
+print 'Building index done!'
+print 'Current time: %s' % ctime()
+
+print '\nComputing features for candidate PSMs for query spectra...'
+results = []
+titles = []
+for title in index.search_index.keys():
+	if len(index.search_index[title]) != 0:
+		titles.append(title)
+length = len(titles)
+print 'Total number of spectra to be searched: %d' % length
+for i in range(0, length):
+	print '%d / %d' % (i, length)
+	sys.stdout.flush()
+	title = titles[i]
+	result = get_matches_per_spec(mass, param, index, title)
+	result = [title, result]
+	results.append(result)
+print 'Computing features done!\n'
+print 'Current time: %s' % ctime()
+
+print '\nScoring and ranking PSMs corresponding to the same query spectrum...'
+print 'Ranking all top-hit PSMs...'
+tophits = get_tophits(index, results)
+print 'Scoring and ranking PSMs done!'
+print 'Current time: %s' % ctime()
+
+print '\nOutputting results...'
+pickle.dump(tophits, file('tophits.pickle', 'w'))
+print 'Writing result to file: %s' % output_file
+write_results(output_file, tophits)
+print 'Outputtting results done!'
+print 'Current time %s' % ctime()
+
+if param['annotation'] == True:
+	for i in range(10): #range(len(tophits)):
+		[match, filename] = get_matches_from_tophits(tophits[i], param, mass, spec_dict)
+		match.annotate(filename, mass)
+
+filter_by_fdr(tophits, param['decoy_string'], param['cutoff'], param['is_unique'])
+
+print 'XLSearch finished running!'
